c7e88e4a8b4f99909daede6f9944a9424c98a855
Peter <chenduona@gmail.com>
2021-05-31 23:19:21 +1000

    use dataframe

--- data.py
+++ data.py
@@ -1,3 +1,4 @@
+#%%
 import bz2
 import json
 import pandas as pd
@@ -9,65 +10,16 @@ import pickle
 import matplotlib.pyplot as plt
 
 
-def process_msg(msg):
-    time = float(msg['pt'])/1000
-    mc = msg['mc']
 
-    for mc1 in mc:
-        market_id = mc1['id']
-
-        initial_add_runner = True
-        if market_id in market_to_runner:
-            initial_add_runner = False
-        else:
-            market_to_runner[market_id] = {
-                'runners' : set()
-            }
-
-        if 'marketDefinition' in mc1:
-            event_id = mc1['marketDefinition']['eventId']
-
-            runners = mc1['marketDefinition']['runners']
-            for r in runners:
-                # recording the runners
-                if r['id'] not in runner_to_market:
-                    runner_to_market[r['id']] = {}
-                    runner_to_market[r['id']][market_id] = {}
-                elif market_id not in runner_to_market[r['id']]:
-                    runner_to_market[r['id']][market_id] = {}
-
-                # recording the winner status
-                if r['status'] == 'WINNER':
-                    runner_to_market[r['id']][market_id]['status'] = True
-                elif r['status'] == 'LOSER':
-                    runner_to_market[r['id']][market_id]['status'] = False
-                
-                # recording the markets
-                if initial_add_runner:
-                    market_to_runner[market_id]['runners'].add(r['id'])
-
-        elif 'rc' in mc1: 
-            for runner in mc1['rc']:
-                runner_id = runner['id']
-                price = float(runner['ltp'])
-                if runner_id in runner_to_market and market_id in runner_to_market[runner_id]:
-                    if len(runner_to_market[runner_id][market_id]) == 0:
-                        runner_to_market[runner_id][market_id]['price'] = pd.Series([price], index=[datetime.fromtimestamp(time)])
-                    else:
-                        runner_to_market[runner_id][market_id]['price'] = runner_to_market[runner_id][market_id]['price'].append(pd.Series([price], index=[datetime.fromtimestamp(time)]))
-                else:
-                    print('runner ', runner_id, ' doesn\'t appear in the market def')
 
 base_dir = './BASIC/2020'
-months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug']
+months = ['Jan']
+# , 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug'
 day_dirs = []
-dates = []
+
 for i, m in enumerate(months):
     _, num_days = calendar.monthrange(2020, i+1)
-    day_dirs += ['/'.join([base_dir, m, str(day)]) for day in range(1, num_days+1)]
-    dates += pd.to_datetime(['/'.join([str(i+1), str(day), '2020']) for day in range(1, num_days+1)])
-
-winnings = []
+    day_dirs += ['/'.join([base_dir, m, str(day)]) for day in range(1, num_days+1)[:1]]
 
 f_names = []
 for d in day_dirs:
@@ -78,74 +30,137 @@ for d in day_dirs:
     else:
         pass
 
-# f_names = ['./BASIC/2020/Jan/1/29636351/29636351.bz2']
+
+#%% Getting the dataframe
+from time import time
 
 
-re_run = False
+def process_msg_by_line(msg, df_runner_market):
+    time = float(msg['pt'])/1000
+    mc = msg['mc']
+
+    for mc1 in mc:
+        marketId = mc1['id']
+
+        if 'marketDefinition' in mc1:
+            event_id = mc1['marketDefinition']['eventId']
+
+            runners = mc1['marketDefinition']['runners']
+            for r in runners:
+                runnerId = r['id']
+                # recording the winner status
+                if str(runnerId) + str(marketId) not in df_runner_market.index:
+                    df_runner_market.loc[str(runnerId) + str(marketId)] = [marketId, runnerId, None, None]
+                
+                if r['status'] == 'WINNER':
+                    df_runner_market.loc[runnerId, marketId]['win'] = True
+                elif r['status'] == 'LOSER':
+                    df_runner_market.loc[str(runnerId) + str(marketId)]['win'] = False
+        elif 'rc' in mc1: 
+            for runner in mc1['rc']:
+                runnerId = runner['id']
+                bid = float(runner['ltp'])
+                if type(df_runner_market.loc[str(runnerId) + str(marketId)]['bid']) != type(None):
+                    df_runner_market.loc[str(runnerId) + str(marketId)]['bid'] = df_runner_market.loc[str(runnerId) + str(marketId)]['bid'].append(pd.Series([bid], index=[datetime.fromtimestamp(time)]))
+                else:
+                    df_runner_market.loc[str(runnerId) + str(marketId)]['bid'] = pd.Series([bid], index=[datetime.fromtimestamp(time)])
+
+
+
+def process_msg_by_line1(msg, df_runner_market):
+    time = float(msg['pt'])/1000
+    mc = msg['mc']
+
+    for mc1 in mc:
+        marketId = mc1['id']
+
+        if 'marketDefinition' in mc1:
+            event_id = mc1['marketDefinition']['eventId']
+
+            runners = mc1['marketDefinition']['runners']
+            for r in runners:
+                runnerId = r['id']
+                # recording the winner status
+                if (runnerId, marketId) not in df_runner_market:
+                    df_runner_market[(runnerId, marketId)] = {}
+                
+                if r['status'] == 'WINNER':
+                    df_runner_market[(runnerId, marketId)]['win'] = True
+                elif r['status'] == 'LOSER':
+                    df_runner_market[(runnerId, marketId)]['win'] = False
+        elif 'rc' in mc1: 
+            for runner in mc1['rc']:
+                runnerId = runner['id']
+                bid = float(runner['ltp'])
+                if 'bid' in df_runner_market[(runnerId, marketId)]:
+                    df_runner_market[(runnerId, marketId)]['bid'] = df_runner_market[(runnerId, marketId)]['bid'].append(pd.Series([bid], index=[datetime.fromtimestamp(time)]))
+                else:
+                    df_runner_market[(runnerId, marketId)]['bid'] = pd.Series([bid], index=[datetime.fromtimestamp(time)])
+
+
+
+re_run = True
+
+df_runner_market = {}
+
+start = time()
 
 for i, f_name in enumerate(f_names):
-    res_fname = '.'.join(f_name.split('.')[:-1]) + '.res'
-    if re_run or not os.path.exists(res_fname):
-        runner_to_market = {}
-        market_to_runner = {}
-
+    if re_run:
         with bz2.open(f_name, 'rb') as bet:
             read = bet.read().decode('utf-8')
             read_lines = [ele.strip() for ele in read.split('\n')]
-            # print()
-            try:
-                for line in read_lines:
-                    if len(line) > 0:
-                        msg = json.loads(line)
-                        process_msg(msg)
-            except Exception:
-                print('there is an exception', line)
+            for line in read_lines:
+                if len(line) > 0:
+                    msg = json.loads(line)
+                    process_msg_by_line1(msg, df_runner_market)
 
-        for market_id, race in market_to_runner.items():
-            last_odds = []
-            runners = race['runners']
-            market_to_runner[market_id]['runners'] = list(runners)
-            for runner in runners:
-                if runner in runner_to_market and market_id in runner_to_market[runner] and 'price' in runner_to_market[runner][market_id]:
-                    last_odds.append({
-                        runner_to_market[runner][market_id]['price'][-1]
-                        })
+runner_market = pd.DataFrame.from_dict(df_runner_market, orient='index')
+runner_market.index.names = ('runnerId', 'marketId')
+
+end = time()
+print('total time spend ', end - start)
+
+#%%
+
+
+
+#%% get the ones with the most favourable odds
+market_list = runner_market.index.unique(level='marketId')
+# runner_list = df_runner_market.runnerId.unique()
+
+winnings = []
+for m in market_list:
+    runner_market.xs(m, level='marketId')
+
+
+#%% Processing the dataframe
+
+if re_run:
+    with open('df_runner_market.pickle', 'wb') as f:
+        pickle.dump(df_runner_market, f)
+else:
+    with open('df_runner_market.pickle', 'rb') as f:
+        [df_runner_market, _] = pickle.load(f)
+
+#%% calculating winnings
+
+winnings = []
+def cal_winnings(df_runner_market):
+    winning = -1
+    for id_r, runner in runner_to_market.items():
+        for id_m, market in runner.items():
+            if 'status' in market and 'fav_odds' in market_to_runner[id_m] and market_to_runner[id_m]['fav_odds'] == id_r:
+                if market['status'] and 'price' in market:
+                    winning += market['price'][-1] 
                 else:
-                    # print('market runner comb doens\'t exist', runner, market_id)
-                    pass
-            if len(last_odds) > 0:
-                fav_odds = np.argmin(last_odds)
-                market_to_runner[market_id]['fav_odds'] = market_to_runner[market_id]['runners'][fav_odds]
-
-        winning = 0
-        for id_r, runner in runner_to_market.items():
-            for id_m, market in runner.items():
-                if 'status' in market and 'fav_odds' in market_to_runner[id_m] and market_to_runner[id_m]['fav_odds'] == id_r:
-                    if market['status'] and 'price' in market:
-                        winning += market['price'][-1] - 1
-                    else:
-                        winning -= 1
-                    # print('winning is: ', winning)
-
-        with open(res_fname, 'wb') as f:
-            pickle.dump([runner_to_market, market_to_runner, winning], f)
-    else:
-        winning = 0
-        # print('Already ran, printing results: ')
-        with open(res_fname, 'rb') as f:
-            [runner_to_market, market_to_runner, _] = pickle.load(f)
-
-        for id_r, runner in runner_to_market.items():
-            for id_m, market in runner.items():
-                if 'status' in market and 'fav_odds' in market_to_runner[id_m] and market_to_runner[id_m]['fav_odds'] == id_r:
-                    if market['status'] and 'price' in market:
-                        winning += market['price'][-1] - 1
-                    else:
-                        winning -= 1
+                    winning -= 0
+    return winning
 
 
-    winnings.append(winning)
-        # print('winning is: ', winning)
+winning = cal_winnings(df_runner_market)
+winnings.append(winning)
+    # print('winning is: ', winning)
 
 with open('./all.res', 'w') as f:
     json.dump(winnings, f)
@@ -158,3 +173,5 @@ plt.show()
 # print(runner_to_market)
 
 # print(winning)
+
+# %%
