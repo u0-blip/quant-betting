9ca71d9bb6e59709dd5f0ab9c988a0f72ca68ea3
Peter <chenduona@gmail.com>
2021-06-02 00:47:29 +1000

    run first classification algorith (so fucking draining, lol)

--- data.py
+++ data.py
@@ -8,7 +8,8 @@ import calendar
 import os
 import pickle
 import matplotlib.pyplot as plt
-
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.multioutput import MultiOutputRegressor
 
 
 
@@ -115,24 +116,89 @@ for i, f_name in enumerate(f_names):
                     msg = json.loads(line)
                     process_msg_by_line1(msg, df_runner_market)
 
-runner_market = pd.DataFrame.from_dict(df_runner_market, orient='index')
-runner_market.index.names = ('runnerId', 'marketId')
+df_runner_market = pd.DataFrame.from_dict(df_runner_market, orient='index')
+df_runner_market.index.names = ('runnerId', 'marketId')
+market_list = df_runner_market.index.unique(level='marketId')
+
 
 end = time()
 print('total time spend ', end - start)
+#%%
+df_runner_market['bid_arr'] = df_runner_market.bid.apply(lambda x: x.to_numpy() if type(x) != float else 0)
+df_runner_market['bid_len'] = df_runner_market.bid_arr.apply(len)
+fill_bid_arr_size = df_runner_market.describe().loc['75%'].values[0] + 5
+df_runner_market['bid_arr_filled'] = df_runner_market.bid_arr.apply(lambda x: np.pad(x, (int(fill_bid_arr_size - len(x)),0), mode='constant', constant_values=0) if fill_bid_arr_size > len(x) else x[int(len(x) - fill_bid_arr_size):])
+
+
+#%%
+# df_runner_market.groupby('marketId').count().bid.sort_values().plot(use_index=False)
+# drop everything with bigger than 15 runners
+fill_runners = df_runner_market.groupby('marketId').count().bid.sort_values().describe()['75%']
+df_runner_market.drop(df_runner_market.groupby('marketId').count().bid[lambda x: x > fill_runners].index, level='marketId')
+combined_market = df_runner_market.groupby('marketId')[['bid_arr_filled', 'win']].apply(lambda x:( np.concatenate(x['bid_arr_filled']), x['win'].values))
+all_filled = fill_runners*fill_bid_arr_size
+
 
 #%%
 
+combined_market = pd.DataFrame.from_dict(dict(zip(combined_market.index, combined_market.values)), orient='index', columns=['bid_arr', 'win'])
+
+combined_market.bid_arr = combined_market.bid_arr.apply(lambda x: np.pad(x, (0, int(all_filled - len(x))), mode='constant', constant_values=0) if all_filled >= len(x) else print('Array too long'))
+
+combined_market.win = combined_market.win.apply(lambda x: np.pad(x.astype(float), (0, int(fill_runners - len(x))), mode='constant', constant_values=0) if fill_runners >= len(x) else print('Array too long'))
 
 
 #%% get the ones with the most favourable odds
-market_list = runner_market.index.unique(level='marketId')
+train_market = combined_market.iloc[:int(len(combined_market)*0.8)]
+test_market = combined_market.iloc[int(len(combined_market)*0.8):]
+train_value = np.vstack(train_market.bid_arr.values)
+train_label = np.vstack(train_market.win.values)
+test_value = np.vstack(test_market.bid_arr.values)
+test_label = np.vstack(test_market.win.values)
+
 # runner_list = df_runner_market.runnerId.unique()
 
-winnings = []
-for m in market_list:
-    runner_market.xs(m, level='marketId')
+#%% use Kneighbors regressor
 
+
+
+knn = KNeighborsRegressor()
+regr = MultiOutputRegressor(knn)
+
+regr.fit(train_value, train_label)
+pred = regr.predict(test_value)
+
+ran = np.random.rand(*test_label.shape)
+mse_rand = ((ran - test_label)**2).mean()
+mse_pred = ((pred - test_label)**2).mean()
+print('rand ', mse_rand, ' pred ', mse_pred)
+
+#%% use xgboost
+import xgboost as xgb
+# read in data
+dtrain = xgb.DMatrix(train_value, train_label)
+dtest = xgb.DMatrix(test_value)
+# specify parameters via map
+param = {'max_depth':2, 'eta':1, 'objective':'binary:logistic' }
+num_round = 2
+
+
+# fitting
+xgbr = xgb.XGBRegressor(base_score=0.5, booster='gbtree', colsample_bylevel=1,
+       colsample_bynode=1, colsample_bytree=1, gamma=0,
+       importance_type='gain', learning_rate=0.1, max_delta_step=0,
+       max_depth=3, min_child_weight=1, missing=None, n_estimators=100,
+       n_jobs=1, nthread=None, objective='reg:squarederror', random_state=0,
+       reg_alpha=0, reg_lambda=1, scale_pos_weight=1, seed=None,
+       silent=None, subsample=1)
+
+multioutputregressor = MultiOutputRegressor(xgbr).fit(train_value, train_label)
+
+# make prediction
+xgb_preds = multioutputregressor.predict(test_value)
+
+mse_pred = ((xgb_preds - test_label)**2).mean()
+print(' xgb pred mse ', mse_pred)
 
 #%% Processing the dataframe
 
